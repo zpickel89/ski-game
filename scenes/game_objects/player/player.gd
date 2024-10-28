a9d5714f08d16e998c3cb16d22c0fcae4fae00f4
Zachary Pickel <zpickel@gmail.com>
2024-10-28 17:20:25 -0500

    Player Camera
    - Added camera to follow the player
    - Added downward force to the player
    - 3 sec of invulnerability to the player on respawn

--- scenes/game_objects/player/player.gd
+++ scenes/game_objects/player/player.gd
@@ -3,8 +3,11 @@ class_name Player
 
 signal died
 
+@export var downward_speed: float  = 1.2
+
 @onready var velocity_component: VelocityComponent = $VelocityComponent
 @onready var collision_area_2d: Area2D = $CollisionArea2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionArea2D/CollisionShape2D
 
 
 func _ready():
@@ -16,10 +19,12 @@ func _process(delta: float) -> void:
 	velocity_component.acclerate_in_direction(move_vector)
 	velocity_component.move(self)
 
+func set_collision_active(is_active: bool = true):
+	collision_shape_2d.disabled = !is_active
 
 func get_movement_vector():
 	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up") + downward_speed
 	return Vector2(x_movement, y_movement)
 	
 	
--- scenes/manager/player_manager.gd
+++ scenes/manager/player_manager.gd
@@ -10,15 +10,20 @@ func _ready():
 	
 	
 func on_player_died():
+	Callable(respawn_player).call_deferred()
+	
+
+func respawn_player():
 	var spawn_position = Vector2.ZERO
 	var new_player_instance = player_scene.instantiate() as Player
+	
 	new_player_instance.global_position = spawn_position
 	get_parent().add_child(new_player_instance)
+	
 	player.queue_free()
-	connect_to_respawned_player(new_player_instance)
-	
-	
-func connect_to_respawned_player(new_player: Player):
-	player = new_player
+	player = new_player_instance
 	player.died.connect(on_player_died)
+	player.set_collision_active(false)
+	await get_tree().create_timer(3).timeout
+	player.set_collision_active(true)
 	
